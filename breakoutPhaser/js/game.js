7aa4c1db18c57f895eae760a8a833e91742c7eae
Alex Yong <alexjyong@gmail.com>
2021-03-03 11:01:49 -0600

    adding in new code

---
+++ breakoutPhaser/js/game.js
@@ -0,0 +1,10 @@
+var game = new Phaser.Game(480,320, Phaser.CANVAS, null,{
+  preload:preload, create: create, update:update
+});
+function preload(){
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally=true;
+  game.scale.pageAlignVertically =true;
+}
+function create(){}
+function update(){}
