578ee9885c9251c4cf28167cc1d310f06125672b
Alex Yong <alexjyong@gmail.com>
2021-03-07 10:19:27 -0500

    adding more stuff

--- breakoutPhaser/js/game.js
+++ breakoutPhaser/js/game.js
@@ -2,8 +2,11 @@ var game = new Phaser.Game(480,320, Phaser.CANVAS, null,{
   preload:preload, create: create, update:update
 });
 var ball;
+var paddle;
+
 
 function preload(){
+
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   game.scale.pageAlignHorizontally=true;
@@ -11,12 +14,33 @@ function preload(){
   game.stage.backgroundColor = '#eee';
 
   game.load.image('ball', 'img/ball.png');
+  game.load.image('paddle', 'img/paddle.png');
 }
+
 function create(){
-  ball = game.add.sprite(50, 50, 'ball');
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25,'ball');
+  ball.anchor.set(0.5);
+  game.physics.enable(ball, Phaser.Physics.ARCADE);
+  ball.body.collideWorldBounds = true;
+  ball.body.velocity.set(150, -150);
+  ball.body.bounce.set(1);
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  paddle.anchor.set(0.5,1);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
+  game.physics.arcade.checkCollision.down = false;
+
+  ball.checkWorldBounds = true;
+  ball.events.onOutOfBounds.add(function(){
+    alert('Game over!');
+    location.reload();
+  }, this);
 }
 
 function update(){
-  ball.x += 1;
-  ball.y += 1;
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width*0.5;
+
 }
